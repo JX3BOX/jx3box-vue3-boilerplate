9c8cc2672cd117634b5b830409b14c053a1b67ea
iRuxu <rx6@qq.com>
2022-02-05 06:31:29 +0800

    feat:resolve config.base as cdn root for production

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,37 @@
-import { defineConfig } from "vite";
+import { defineConfig,loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
+import getStaticPath from './helper/static_path'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        // Use @ as ./src in template/css/js
-        alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
-    },
-    css: {
-        preprocessorOptions: {
-            // scss: {
-            //     additionalData: `$injectedColor: orange;`,
-            // },
-            less : {
-                // less mixin , please reload serve when these files change
-                globalVars: {
-                    hack: `true; 
-                    @import "./node_modules/csslab/base.less";
-                    @import "./node_modules/@jx3box/jx3box-common/css/var.less";
-                    @import "./src/assets/css/var.less";`,
-                }
-            }
+export default ({ command, mode }) => {
+
+    return defineConfig({
+    
+        //Define static files path
+        base: mode == "development" ? "/" : getStaticPath(loadEnv(mode, process.cwd()).VITE_STATIC_PATH),
+    
+        resolve: {
+            // Use @ as ./src in template/css/js
+            alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
         },
-    },
-});
+    
+        css: {
+            preprocessorOptions: {
+                // scss: {
+                //     additionalData: `$injectedColor: orange;`,
+                // },
+                less: {
+                    // less mixin , please reload serve when these files change
+                    globalVars: {
+                        hack: `true; 
+                        @import "./node_modules/csslab/base.less";
+                        @import "./node_modules/@jx3box/jx3box-common/css/var.less";
+                        @import "./src/assets/css/var.less";`,
+                    },
+                },
+            },
+        },
+    
+        plugins: [vue()],
+    });
+}
